d5437f7ee3be96eab19c932e3eea95c6541b3a45
Chris Anderson <chanderson0@gmail.com>
2018-12-30 11:30:46 -0500

    optimize build size, remove old deps

--- sketches/0022/src/index.ts
+++ sketches/0022/src/index.ts
@@ -1,15 +1,10 @@
 import * as THREE from "three";
-// @ts-ignore
-import * as ImageTracer from "./vendor/imagetracer_v1.2.4.js";
+
 import EffectComposer, {
   RenderPass,
-  ShaderPass,
-  CopyShader
+  ShaderPass
 } from "three-effectcomposer-es6";
 import * as CCapture from "ccapture.js";
-import * as Color from "color";
-// @ts-ignore
-// import * as FXAAShader from "three-shader-fxaa";
 
 const recorder = new CCapture({
   name: "Sketch-0022",
@@ -21,10 +16,6 @@ const recorder = new CCapture({
 import glsl = require("glslify");
 const unpackVert = glsl.file("./shaders/unpack.vert.glsl");
 const unpackFrag = glsl.file("./shaders/unpack.frag.glsl");
-
-const fmod = function(a: number, b: number) {
-  return a - Math.floor(a / b) * b;
-};
 
 const SCREEN_BG_COLOR = 0xff985c;
 const BG_COLOR = 0xffe66a;
@@ -39,19 +30,7 @@ const FXAAShader = {
   fragmentShader: glsl.file("./shaders/three-shader-fxaa/frag.glsl")
 };
 
-const SHADOW = `
-<defs>
-  <filter id="shadow" x="0" y="0">
-    <feOffset result="offOut" in="SourceAlpha" dx="-5" dy="-5" />
-    <feGaussianBlur result="blurOut" in="offOut" stdDeviation="3" />
-    <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
-  </filter>
-</defs>
-`;
-
 const app = document.getElementById("app") as HTMLElement;
-let svgElement: SVGElement | null = null;
-let palette: any = undefined;
 
 const SCALE = 2.0;
 const OUTPUT_WIDTH = 724 * SCALE;
@@ -162,15 +141,9 @@ class OrthoDepthStripes {
 const target = new THREE.WebGLRenderTarget(OUTPUT_WIDTH, OUTPUT_HEIGHT);
 const composer = new EffectComposer(renderer, target);
 composer.addPass(new RenderPass(scene, camera));
-composer.addPass(new OrthoDepthStripes(camera, scene, BG_COLOR, FG_COLOR, SCREEN_BG_COLOR));
-
-// const fxaa = new ShaderPass(FXAAShader);
-// console.log(fxaa);
-// fxaa.uniforms.resolution = {
-//   value: new THREE.Vector2(OUTPUT_WIDTH, OUTPUT_HEIGHT)
-// };
-// fxaa.renderToScreen = true;
-// composer.addPass(fxaa);
+composer.addPass(
+  new OrthoDepthStripes(camera, scene, BG_COLOR, FG_COLOR, SCREEN_BG_COLOR)
+);
 
 const effectFXAA = new ShaderPass(FXAAShader);
 effectFXAA.uniforms["resolution"].value.set(
@@ -179,32 +152,6 @@ effectFXAA.uniforms["resolution"].value.set(
 );
 effectFXAA.renderToScreen = true;
 composer.addPass(effectFXAA);
-
-// let i = 0;
-// for (let x = -0.4; x <= 0.4; x += 0.25) {
-//   for (let y = -0.3; y <= 0.3; y += 0.25) {
-//     const geometry = new THREE.TorusGeometry(0.01, 0.005);
-//     // const edges = new THREE.EdgesGeometry(geometry as any, 5);
-//     // const line = new THREE.LineSegments(
-//     //   edges,
-//     //   new THREE.LineBasicMaterial({ color: 0x0 })
-//     // );
-//     const material = new THREE.MeshBasicMaterial({
-//       color: Math.random() * 0xffffff,
-//       wireframe: false
-//     });
-//     const cube = new THREE.Mesh(geometry, material);
-
-//     cube.rotateX(x * Math.PI * 1.0);
-//     cube.rotateY(y * Math.PI * 1.0);
-//     cube.rotateZ((i / 1000) * Math.PI);
-//     cube.position.set(x, y, i * 10);
-
-//     scene.add(cube);
-
-//     ++i;
-//   }
-// }
 
 const meshes: THREE.Mesh[] = [];
 const NUM = 100;
@@ -223,12 +170,6 @@ for (let i = 0; i < NUM; ++i) {
   scene.add(mesh);
   meshes.push(mesh);
 }
-
-const webglCanvas = renderer.domElement;
-const offscreenCanvas = document.createElement("canvas");
-offscreenCanvas.width = webglCanvas.width;
-offscreenCanvas.height = webglCanvas.height;
-const offscreenCtx = offscreenCanvas.getContext("2d");
 
 let recording = false;
 const RECORDING_FPS = 60;
@@ -262,200 +203,19 @@ function loop() {
   for (let i = 0; i < NUM; ++i) {
     const mesh = meshes[i];
     const x = mesh.position.x;
-    // mesh.position.x = (frac - 0.5) * 0.6;
-    // mesh.position.y = Math.sin(time + i) * 0.05;
 
     mesh.rotation.x = ((time * 0.4 + x * 3.0) % 1.0) * Math.PI * 2;
     mesh.scale.setScalar(1.0 - (1.0 - Math.abs(mesh.position.x / 0.7)) / 3.0);
-    // mesh.rotation.z = ((time * 0.2 + i * 0.1) % 1.0) * Math.PI * 2;
   }
 
   // Render
-  // renderer.render(scene, camera, target);
-  // renderer.render(postScene, postCamera);
-  // // renderer.render(scene, camera);
-  render();
-
-  // const imgData = ImageTracer.getImgdata(renderer.domElement);
-  // const svg = ImageTracer.imagedataToSVG(imgData);
+  composer.render();
 
   if (recording) {
     recorder.capture(renderer.domElement);
-    // downloadPNG(canvas => {
-    //   recorder.capture(canvas);
-    // });
   }
 }
 loop();
-
-function render() {
-  // renderer.render(scene, camera, target);
-  // renderer.render(postScene, postCamera);
-
-  composer.render();
-
-  const no = false;
-  if (no && offscreenCtx) {
-    offscreenCtx.drawImage(webglCanvas, 0, 0);
-    const imgData = offscreenCtx.getImageData(
-      0,
-      0,
-      offscreenCanvas.width,
-      offscreenCanvas.height
-    );
-
-    // const out = Potrace.fromImage(offscreenCanvas, {});
-    // const svgString = out.toSVG(1.0 / SCALE);
-    // console.log(svg);
-    // if (!updatedOptions) {
-    const options = { colorquantcycles: 3 };
-    // }
-    if (!palette) {
-      palette = ImageTracer.samplepalette2(12, imgData);
-      console.log(palette);
-    }
-    const svgString = ImageTracer.imagedataToSVG(imgData, {
-      ...options,
-      pal: palette
-    });
-
-    const holder = document.createElement("div");
-    holder.innerHTML = svgString;
-    svgElement = holder.firstChild as SVGElement;
-
-    svgElement.style.width = "100%";
-    svgElement.style.height = "100%";
-    svgElement.setAttribute("viewBox", `0 0 ${OUTPUT_WIDTH} ${OUTPUT_HEIGHT}`);
-
-    app.innerHTML = "";
-    app.appendChild(svgElement);
-
-    postprocessSvg(svgElement);
-  }
-}
-
-function postprocessSvg(elem: SVGElement) {
-  const elemsToRemove: SVGElement[] = [];
-  const elemsToKeep = [];
-  // elem.setAttribute(
-  //   "style",
-  //   (elem.getAttribute("style") || "") + ";background:black"
-  // );
-  for (let i = 0; i < elem.children.length; ++i) {
-    const child = elem.children[i];
-    if (child instanceof SVGElement) {
-      child.setAttribute("stroke", child.getAttribute("fill") as string);
-      child.setAttribute("stroke-width", "2");
-
-      const fill = child.getAttribute("fill");
-      if (fill) {
-        if (fill === "rgb(0,0,0)") {
-          elemsToRemove.push(child);
-          continue;
-        }
-
-        const c = new Color(fill);
-        const lightness = c.lightness();
-        const newFill = new Color(0xffffff);
-        // child.setAttribute("fill");
-      }
-
-      // child.setAttribute("stroke", "white");
-      // child.setAttribute("stroke-width", "2");
-
-      child.setAttribute("stroke-linejoin", "round");
-      child.setAttribute("stroke-miterlimit", "0");
-      // child.setAttribute("stroke-alignment", "outer");
-
-      // child.setAttribute("stroke", "black");
-
-      // child.setAttribute("fill", "none");
-
-      const rect = child.getBoundingClientRect();
-      const area = rect.width * rect.height;
-      // if (area < 100 * SCALE) {
-      //   elemsToRemove.push(child);
-      //   continue;
-      // }
-      const d = child.getAttribute("d");
-      if (d && d.startsWith("M 0 0")) {
-        elemsToRemove.push(child);
-        continue;
-      }
-
-      elemsToKeep.push(child);
-    }
-  }
-
-  for (const e of elemsToRemove) {
-    elem.removeChild(e);
-  }
-
-  // const groupElem = document.createElement("g");
-  // for (const e of elemsToKeep) {
-  //   groupElem.appendChild(e);
-  // }
-  // groupElem.setAttribute("filter", "url(#shadow)");
-  // elem.innerHTML = SHADOW;
-  // elem.innerHTML += '<path fill="black" d="M 0 0"></path>';
-  // elem.appendChild(groupElem);
-}
-
-function downloadData(data: string, filename: string) {
-  const link = document.createElement("a");
-  link.href = data;
-  link.download = filename;
-  link.click();
-}
-
-function downloadSVG() {
-  if (!svgElement) return;
-
-  svgElement.setAttribute("version", "1.1");
-  svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  const viewBox = svgElement.getAttribute("viewBox") || "";
-  svgElement.removeAttribute("viewBox");
-
-  const blob = new Blob([svgElement.outerHTML], {
-    type: "image/svg+xml;charset=utf-8"
-  });
-  const data = URL.createObjectURL(blob);
-  downloadData(data, "capture.svg");
-
-  svgElement.setAttribute("viewBox", viewBox);
-}
-
-function downloadPNG(callback: (c: HTMLCanvasElement) => void) {
-  if (!svgElement) return;
-
-  svgElement.setAttribute("version", "1.1");
-  svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  // const viewBox = svgElement.getAttribute("viewBox") || "";
-  // svgElement.removeAttribute("viewBox");
-
-  const svgData = new XMLSerializer().serializeToString(svgElement);
-  const canvas = document.createElement("canvas");
-  // const svgSize = svgElement.getBoundingClientRect();
-  // const viewBox = svgElement.getAttribute("viewBox") || "";
-  // svgElement.removeAttribute("viewBox");
-
-  canvas.width = 724 * 2;
-  canvas.height = 512 * 2;
-
-  const ctx = canvas.getContext("2d");
-  if (!ctx) return;
-
-  const img = document.createElement("img");
-  img.setAttribute("src", "data:image/svg+xml;base64," + btoa(svgData));
-
-  img.onload = function() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-    callback(canvas);
-  };
-}
 
 const startRecording = () => {
   recording = true;
@@ -479,12 +239,6 @@ window.addEventListener("keypress", e => {
     } else {
       startRecording();
     }
-  } else if (e.key == "s") {
-    downloadSVG();
-  } else if (e.key == "p") {
-    downloadPNG(canvas => {
-      downloadData(canvas.toDataURL("image/png"), "capture.png");
-    });
   } else if (e.key == " ") {
     stop();
   }
--- sketches/0022/src/types/potrace-js.d.ts
+++
@@ -1,1 +0,0 @@
-// declare module 'potrace-js';
